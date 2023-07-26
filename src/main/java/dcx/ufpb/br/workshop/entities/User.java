7db196fc16ef45a843401343043543d6afe19033
RonyAbreu <ronyelison78@gmail.com>
2023-07-26 17:31:10 -0300

    gerando hashcode and equals na clase User

--- src/main/java/dcx/ufpb/br/workshop/entities/User.java
+++ src/main/java/dcx/ufpb/br/workshop/entities/User.java
@@ -1,2 +1,35 @@
-package dcx.ufpb.br.workshop.entities;public class User {
+package dcx.ufpb.br.workshop.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User implements Serializable {
+    private Long id;
+    private String nome;
+    private String email;
+    private String phone;
+    private String password;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
