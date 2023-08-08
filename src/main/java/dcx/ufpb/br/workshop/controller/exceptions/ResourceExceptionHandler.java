77bf30c38ae8a40c241b8217525434c748e057e3
RonyAbreu <ronyelison78@gmail.com>
2023-08-08 16:59:52 -0300

    criação da exceção do método update, na classe UserService

--- src/main/java/dcx/ufpb/br/workshop/controller/exceptions/ResourceExceptionHandler.java
+++ src/main/java/dcx/ufpb/br/workshop/controller/exceptions/ResourceExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.xml.crypto.Data;
 import java.time.Instant;
 
 @ControllerAdvice
